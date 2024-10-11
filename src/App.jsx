be5549a3395bb116eae51a56a3ffd6795e4167e4
Esaint <esaint.song@gmail.com>
2024-10-12 00:44:10 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,10 @@
 import About from "./components/About";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
-import SkillCard from "./components/SkillCard";
+import Review from "./components/Review";
 import Skills from "./components/Skills";
 import Work from "./components/Work";
+import Contact from "./components/Contact";
 
 const App = () => {
   return (
@@ -19,6 +20,8 @@ const App = () => {
         <About />
         <Skills /> 
         <Work /> 
+        <Review />
+        <Contact />
       </main>
     </>
   );
---
+++ src/components/Contact.jsx
@@ -0,0 +1,15 @@
+/**
+ * @copyright 2024 esaintmjay
+ * @license Apache-2.0
+ */
+
+
+
+
+const Contact = () => {
+  return (
+    <div>Contact</div>
+  )
+}
+
+export default Contact
